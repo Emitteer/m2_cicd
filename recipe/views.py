10efe361ceb4a49fe65dc2bd77935c603ebf4924
emitterr <109480385+Emitteer@users.noreply.github.com>
2024-05-31 15:56:05 +0300

    added view for recipe_detail

--- recipe/views.py
+++ recipe/views.py
@@ -7,3 +7,7 @@ def main(request):
     end_date = datetime.datetime(2023, 12, 31, 23, 59, 59)
     recipes = Recipe.objects.filter(created_at__range=(start_date, end_date))
     return render(request, 'main.html', {'recipes': recipes})
+
+def recipe_detail(request, recipe_id):
+    recipe = get_object_or_404(Recipe, id=recipe_id)
+    return render(request, 'recipe/recipe_detail.html', {'recipe': recipe})
