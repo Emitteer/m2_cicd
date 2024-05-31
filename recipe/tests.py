8bc39855e8144f506c30e3c8e76f662734d62f47
emitterr <109480385+Emitteer@users.noreply.github.com>
2024-05-31 15:35:40 +0300

    Added tests for main_view

--- recipe/tests.py
+++ recipe/tests.py
@@ -7,8 +7,8 @@ class RecipeViewTests(TestCase):
 
     def setUp(self):
         category = Category.objects.create(name='Desserts')
-        Recipe.objects.create(title='Test Recipe 1', description='Description 1', instructions='Instructions 1', ingredients='Ingredients 1', created_at=datetime.datetime(2023, 5, 17), category=category)
-        Recipe.objects.create(title='Test Recipe 2', description='Description 2', instructions='Instructions 2', ingredients='Ingredients 2', created_at=datetime.datetime(2023, 6, 18), category=category)
+        self.recipe1 = Recipe.objects.create(title='Test Recipe 1', description='Description 1', instructions='Instructions 1', ingredients='Ingredients 1', created_at=datetime.datetime(2023, 5, 17), category=category)
+        self.recipe2 = Recipe.objects.create(title='Test Recipe 2', description='Description 2', instructions='Instructions 2', ingredients='Ingredients 2', created_at=datetime.datetime(2023, 6, 18), category=category)
 
     def test_main_view(self):
         response = self.client.get(reverse('main'))
