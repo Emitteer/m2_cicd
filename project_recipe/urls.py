cf25c72c11bb371faf448574348229a407e780c8
emitterr <109480385+Emitteer@users.noreply.github.com>
2024-05-31 15:55:43 +0300

    changed urls

--- project_recipe/urls.py
+++ project_recipe/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 from recipe import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.main, name='main'),
+    path('', include('recipe.urls')),
 ]
