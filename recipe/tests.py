a7cafc7bd490c4f83b6d28e23ae9ba7ce4d66271
emitterr <109480385+Emitteer@users.noreply.github.com>
2024-05-31 15:26:33 +0300

    Initial commit

--- recipe/tests.py
+++ recipe/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Recipe, Category
+import datetime
 
-# Create your tests here.
+class RecipeViewTests(TestCase):
+
+    def setUp(self):
+        category = Category.objects.create(name='Desserts')
+        Recipe.objects.create(title='Test Recipe 1', description='Description 1', instructions='Instructions 1', ingredients='Ingredients 1', created_at=datetime.datetime(2023, 5, 17), category=category)
+        Recipe.objects.create(title='Test Recipe 2', description='Description 2', instructions='Instructions 2', ingredients='Ingredients 2', created_at=datetime.datetime(2023, 6, 18), category=category)
+
+    def test_main_view(self):
+        response = self.client.get(reverse('main'))
+        self.assertEqual(response.status_code, 200)
+        self.assertContains(response, 'Test Recipe 1')
+        self.assertContains(response, 'Test Recipe 2')
