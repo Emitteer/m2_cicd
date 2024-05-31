60a86f42f22598399e76f85edc7e3699ca59aeff
emitterr <109480385+Emitteer@users.noreply.github.com>
2024-05-31 16:02:00 +0300

    fixed recipe detail path

--- recipe/views.py
+++ recipe/views.py
@@ -3,11 +3,12 @@ from .models import Recipe
 import datetime
 
 def main(request):
-    start_date = datetime.datetime(2023, 1, 1)
-    end_date = datetime.datetime(2023, 12, 31, 23, 59, 59)
+    start_date = datetime.datetime(2024, 1, 1)
+    end_date = datetime.datetime(2024, 12, 31, 23, 59, 59)
     recipes = Recipe.objects.filter(created_at__range=(start_date, end_date))
     return render(request, 'main.html', {'recipes': recipes})
 
+
 def recipe_detail(request, recipe_id):
     recipe = get_object_or_404(Recipe, id=recipe_id)
-    return render(request, 'recipe/recipe_detail.html', {'recipe': recipe})
+    return render(request, 'recipe_detail.html', {'recipe': recipe})
